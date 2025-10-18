9035b64f93119ce5bfca6229d08e61f3532b6219
Nirasha10 <nirashaprabashi01@gmail.com>
2025-10-18 22:21:58 +0300

    Update week5_task1_file_io.c

--- src/week5_task1_file_io.c
+++ src/week5_task1_file_io.c
@@ -12,13 +12,40 @@ int main(void) {
     char line[256];
 
     // TODO: 1. Open file for writing (mode = "w")
+    fp = fopen(filename, "w");
+    
     // TODO: 2. Check if file opened successfully
+    if (fp == NULL) {
+        printf("Error opening file for writing!\n");
+        return 1;
+    }
+    
+    printf("Writing lines to data.txt...\n");
+    
     // TODO: 3. Write 2–3 lines of text to the file using fprintf()
+    fprintf(fp, "Hello, file I/O in C!\n");
+    fprintf(fp, "This is another line.\n");
+    
     // TODO: 4. Close the file
+    fclose(fp);
 
     // TODO: 5. Open file again for reading (mode = "r")
+    fp = fopen(filename, "r");
+    
+    if (fp == NULL) {
+        printf("Error opening file for reading!\n");
+        return 1;
+    }
+    
+    printf("Reading contents:\n");
+    
     // TODO: 6. Use fgets() in a loop to read and print each line to the console
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+    
     // TODO: 7. Close the file
+    fclose(fp);
 
     // BONUS: ask user for filename instead of using default "data.txt"
     // BONUS: count number of lines read
